19610275318f2b7b04e2eb7311fea9994670cb60
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-10-22 21:38:02 -0500

    Create App.js
    Learning takeaways: You can pass the value to the header and you need to handle it in the function by using props as a parameter. You can set default props if you want.

---
+++ App.js
@@ -0,0 +1,11 @@
+import Header from './components/Header' // import from Header.js
+
+function App() {
+  return (
+    <div className='container'>
+      <Header title='Bye' /> 
+    </div>
+  );
+}
+
+export default App;
