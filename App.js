afcda04c73106dcf5034c0e1f4e1b850e3b60a06
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-10-23 12:48:28 -0500

    Update App.js
    add useState and array into App.js

--- App.js
+++ App.js
@@ -1,9 +1,39 @@
 import Header from './components/Header' // import from Header.js
+import Tasks from './components/Tasks' // import from Tasks.js
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Finish NTWK Lab 5',
+      day: 'Oct 23rd at 11:59pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Finish Firesides and Schedule Page',
+      day: 'Oct 23rd at 11:59am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Finish React JS Crash Course',
+      day: 'Oct 23rd at 11:59pm',
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: 'Clean the espresso machine',
+      day: 'Oct 24th at 11:59pm',
+      reminder: false,
+    }
+  ])
+
   return (
     <div className='container'>
-      <Header title='Bye' /> 
+      <Header />
+      <Tasks tasks={task} />
     </div>
   );
 }
