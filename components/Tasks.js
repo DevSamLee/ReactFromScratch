494eef2c76949622b0d1cf794b4f8b2046527767
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-10-23 12:47:34 -0500

    Update Tasks.js
    move useState and array to App.js

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,32 +1,4 @@
-import { useState } from 'react'
-
 const Tasks = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Finish NTWK Lab 5',
-      day: 'Oct 23rd at 11:59pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Finish Firesides and Schedule Page',
-      day: 'Oct 23rd at 11:59am',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Finish React JS Crash Course',
-      day: 'Oct 23rd at 11:59pm',
-      reminder: true,
-    },
-    {
-      id: 4,
-      text: 'Clean the espresso machine',
-      day: 'Oct 24th at 11:59pm',
-      reminder: false,
-    }
-  ])
   // to update the tasks -> use SetTasks
   return (
     <>
