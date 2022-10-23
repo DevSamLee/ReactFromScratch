5280f8c314062c760038d931557519043122033e
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-10-23 12:45:58 -0500

    Update Tasks.js
    Learning takeaways: useState for adding a 'tasks' array.

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,33 +1,33 @@
-const tasks = [
-  {
-    id: 1,
-    text: 'Finish NTWK Lab 5',
-    day: 'Oct 23rd at 11:59pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Finish Firesides and Schedule Page',
-    day: 'Oct 23rd at 11:59am',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Finish React JS Crash Course',
-    day: 'Oct 23rd at 11:59pm',
-    reminder: true,
-  },
-  {
-    id: 4,
-    text: 'Clean the espresso machine',
-    day: 'Oct 24th at 11:59pm',
-    reminder: false,
-  }
-]
-// use .map() to iterate the array
-// make sure use {} to assign
-// use id for key value (to prevent warning)
+import { useState } from 'react'
+
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Finish NTWK Lab 5',
+      day: 'Oct 23rd at 11:59pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Finish Firesides and Schedule Page',
+      day: 'Oct 23rd at 11:59am',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Finish React JS Crash Course',
+      day: 'Oct 23rd at 11:59pm',
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: 'Clean the espresso machine',
+      day: 'Oct 24th at 11:59pm',
+      reminder: false,
+    }
+  ])
+  // to update the tasks -> use SetTasks
   return (
     <>
       {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
