4c8e775db7d82663f210796bb09d9e8121dbc0bc
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-10-25 15:48:00 -0500

    Update Tasks.js
    Learning takeaways: when you add a component, you should have it as a parameter

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,11 +1,12 @@
 import Task from './Task'
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   // to update the tasks -> use SetTasks
   return (
     <>
       {tasks.map((task) => (
-      <Task key={task.id} task={task} onDelete={onDelete} />
+      <Task key={task.id} task={task} onDelete={onDelete}
+      onToggle={onToggle} />
       ))}
     </>
   )
