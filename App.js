d514f3e7e9f01fa5c33123858ebe4747fea118a6
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-11-07 23:11:31 -0600

    Update App.js
    Learning takeaways: I can create mock backend component with tasks array and fetch them by using useEffect();

--- App.js
+++ App.js
@@ -1,37 +1,23 @@
 import Header from './components/Header' // import from Header.js
 import Tasks from './components/Tasks' // import from Tasks.js
 import AddTask from './components/AddTask' // import from AddTask.js
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   // true - shows form, false - hides form
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Finish NTWK Lab 5',
-      day: 'Oct 23rd at 11:59pm',
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: 'Finish Firesides and Schedule Page',
-      day: 'Oct 23rd at 11:59am',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Finish React JS Crash Course',
-      day: 'Oct 23rd at 11:59pm',
-      reminder: true,
-    },
-    {
-      id: 4,
-      text: 'Clean the espresso machine',
-      day: 'Oct 24th at 11:59pm',
-      reminder: false,
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+      console.log(data)
     }
-  ])
+
+    fetchTasks()
+  }, [])
   
   // Add Task
   const addTask = (task) => {
