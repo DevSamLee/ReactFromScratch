1f681bc0109ae84d21159a69b76ef0fb42a11ec4
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-11-11 23:24:47 -0600

    Update App.js
    Learning takeaways: fetch can be added to another hook (in this case getTasks())

--- App.js
+++ App.js
@@ -9,15 +9,20 @@ function App() {
   const [tasks, setTasks] = useState([])
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks')
-      const data = await res.json()
-
-      console.log(data)
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-
-    fetchTasks()
+    getTasks()
   }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
   
   // Add Task
   const addTask = (task) => {
@@ -37,7 +42,6 @@ function App() {
     id ? {...task, reminder: !task.reminder } : task))
   }
 
-  // but it's not working? why??
   // need to add props or deconstructure it in Tasks.js
   // ternery operator without else - && 
   return (
